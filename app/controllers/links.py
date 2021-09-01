6c43df25e2fc3a4c3d6835aad0d8d8f5a4999076
nelson.tremea <nelson.tremea@estudante.ifro.edu.br>
2021-09-01 15:54:02 -0400

    API: Links inseridos no banco de dados

--- app/controllers/links.py
+++ app/controllers/links.py
@@ -1,7 +1,24 @@
 from app import app, db
-from flask import render_template
+from flask import render_template, request
 from app.models.tables import Usuario, Link
 
 @app.route('/', methods=["GET", "POST"])
 def inicio():
-    return render_template("base.html")
+    return render_template("link.html")
+
+@app.route('/link', methods=["POST"])
+def enviar_link():
+
+    linkOriginal = request.form["linkOriginal"]
+    linkEncurtado = request.form["linkEncurtado"]
+
+    link = Link(
+        linkOriginal=linkOriginal,
+        linkEncurtado=linkEncurtado
+    )
+    db.session.add(link)
+    db.session.commit()
+
+    linkPronto = Link.query.filter(Link.linkOriginal.like(linkOriginal)).first()
+
+    return render_template("link_pronto.html", linkPronto=linkPronto)
